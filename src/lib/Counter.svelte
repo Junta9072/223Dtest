666c4266ae39744af48b4a668c0add05da0e0b39
Junta9072 <legoblok2709@gmail.com>
2023-02-28 01:58:47 +0100

    trying movement, need to counterbalance for some values

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -11,7 +11,7 @@
   for (let i = 0; i < amount; i++) {
     //setup ball props
    
-      ballArray.push({x: Math.random() * area - area /2, y: Math.random() * area - area /2, z: Math.random()* 50, r: 60, colour: Math.floor(Math.random()*16777215).toString(16)})
+      ballArray.push({x: Math.random() * area - area /2, y: Math.random() * area - area /2, z: Math.random()* 100, r: Math.random() * 30 + 30, colour: Math.floor(Math.random()*16777215).toString(16)})
 }
 
 ballArray.sort(function(a,b){return a.y - b.y})
@@ -19,14 +19,19 @@ ballArray.sort(function(a,b){return a.y - b.y})
   onMount(() => {
     const sketchFunc = (p) => {
       // Your p5.js sketch code goes here
+      let dragStartX;
+      let isDragging = false;
+
+      let camX = 0;
+      let camY = 0;
+      let camSpeed = 5;
       
       p.setup = () => {
         p.angleMode(p.DEGREES);
         p.noStroke()
         p.createCanvas(400, 400, p.WEBGL);
         p.background(200); 
-        p.camera(0, 0, -1000);
-        
+               
       };
 
     
@@ -39,7 +44,17 @@ ballArray.sort(function(a,b){return a.y - b.y})
         p.angleMode(p.DEGREES);
         p.noStroke()
         
-        p.rotate(angle);
+        p.translate(-p.width / 2,-p.height / 2)
+        p.fill('red')
+        p.ellipse(p.width / 2,p.height / 2,10,10)
+      
+        
+    
+        p.rotate(angle)
+        p.translate(camX,camY)
+
+
+        
         //shadow trick example
           //save canvas state
           p.push();
@@ -60,26 +75,60 @@ ballArray.sort(function(a,b){return a.y - b.y})
           item.rectY = p.sin(angle) * item.x + p.cos(angle) * item.y + p.height/2;
         })
         ballArray.sort(function(a,b) {return a.rectY-b.rectY})
-        console.log(ballArray[0].colour,ballArray[1].colour)
+
         ballArray.forEach((item,i) => {
           p.push()
           //draw shadow
           p.fill('gray');
+        
           p.translate(item.x,item.y);
           p.rotate(-angle);
-          p.ellipse(0,item.z,item.r,item.r);
-          //draw ball
-          p.fill('#' + item.colour);
-          p.ellipse(0,0,item.r,item.r)
+          p.translate(p.width / 2 - camX, p.height / 2 - camY);
+
+          p.ellipse(0,0,item.r,item.r);
         p.pop()
-        })        
-     
-        angle+=0.5;
+        })
+
+        ballArray.forEach((item,i) => {
+          p.push()
+          //draw shadow
+         p.translate(item.x,item.y);
+          p.rotate(-angle);
+          p.translate(p.width / 2 - camX, p.height / 2 - camY);
+          p.fill('#' + item.colour);
+          p.ellipse(0,-item.z,item.r,item.r)
+        p.pop()
+        })  
+        
+        if (p.keyIsDown(p.LEFT_ARROW)) {
+    camX += camSpeed;
+  } else if (p.keyIsDown(p.RIGHT_ARROW)) {
+    camX -= camSpeed;
+  } else if (p.keyIsDown(p.UP_ARROW)) {
+    camY += camSpeed;
+  } else if (p.keyIsDown(p.DOWN_ARROW)) {
+    camY -= camSpeed;
+  }   
+
       }
+
+p.mousePressed = () => {
+  dragStartX = p.mouseX;
+  isDragging = true;
+}
+
+p.mouseReleased = () => {
+  isDragging = false;
+}
+
+p.mouseDragged = () => {
+  if (isDragging) {
+    let dragDistX = p.mouseX - dragStartX;
+    angle -= dragDistX;
+    dragStartX = p.mouseX;
+  }
+}
     };
-
-    
-
     sketch = new p5(sketchFunc);
   });
 
